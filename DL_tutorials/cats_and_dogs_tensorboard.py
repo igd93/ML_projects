5a44c2f2c164a7ccdcc9569e4ef49ca925702156
igd93 <102153262+igd93@users.noreply.github.com>
2022-06-15 22:37:29 +0200

    Misc

--- DL_tutorials/cats_and_dogs_tensorboard.py
+++ DL_tutorials/cats_and_dogs_tensorboard.py
@@ -5,34 +5,45 @@ from tensorflow.python.keras.callbacks import TensorBoard
 import time
 import pickle
 
-NAME = 'Cats-vs-dogs-cnn-64x2-{}'.format(int(time.time()))
 
-tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
+dense_layers = [0, 1, 2]
+layer_sizes = [32, 64, 128]
+conv_layers = [1, 2, 3]
 
-#Github won't allow to upload this, they are from cats_and_dogs.ipynb, just saved
 X = pickle.load( open( "X.pickle", 'rb') )
 y = pickle.load( open( "y.pickle", 'rb') )
 
 X = X/255.0
 
-model = Sequential()
-model.add(Conv2D(64, (3, 3), input_shape=X.shape[1:]))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+for dense_layer in dense_layers:
+    for layer_size in layer_sizes:
+        for conv_layer in conv_layers:
+            NAME = '{}-conv-{}-nodes-{}-dense'.format(conv_layer, layer_size, dense_layer, int(time.time()))
 
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+            tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
+            model = Sequential()
 
-model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation('relu'))
+            model.add(Conv2D(layer_size, (3, 3), input_shape=X.shape[1:]))
+            model.add(Activation('relu'))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+            for l in range(conv_layer-1):
+                model.add(Conv2D(layer_size, (3, 3)))
+                model.add(Activation('relu'))
+                model.add(MaxPooling2D(pool_size=(2, 2)))
+            
+            model.add(Flatten())
 
-model.compile(loss='binary_crossentropy',
-              optimizer='adam', metrics=['accuracy'])
+            for l in range(dense_layer):
+                model.add(Dense(layer_size))
+                model.add(Activation('relu'))           
 
+            model.add(Dense(1))
+            model.add(Activation('sigmoid'))
 
-model.fit(X, y, batch_size=32, epochs=5, validation_split=0.1, callbacks = [tensorboard])
+            model.compile(loss='binary_crossentropy',
+                        optimizer='adam', metrics=['accuracy'])
+            
+            
+
+            model.fit(X, y, batch_size=32, epochs=10, validation_split=0.3, callbacks = [tensorboard])
